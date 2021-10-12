8f535917942a0895b5a9ccea5cce21f03b50cd02
Philipp Spiess <hello@philippspiess.com>
2021-10-12 22:53:39 +0100

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by philippspiess on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
